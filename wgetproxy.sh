a460fe18783a157c94f1c29d0cb4e053f9bc4947
danielpnsc <danielpnsc@gmail.com>
2016-07-25 23:16:47 +0200

    Update wgetproxy.sh

--- wgetproxy.sh
+++ wgetproxy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+export http_proxy=http://proxyss.wits.ac.za
+
 user=`echo $USER`
 
 read -sp "Password for $user:" passvar
 echo
-read -p "Download:" downvar
+#read -p "Download:" downvar
 echo
-wget --proxy-user \"'students\'$user\" --proxy-password \"$passvar\" $downvar
+wget --proxy-user 'students\'$user --proxy-password $passvar $1
+
+unset http_proxy
