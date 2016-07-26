cd17591f26299d67815053f567ae198610352968
danielpnsc <danielpnsc@gmail.com>
2016-07-26 15:42:36 +0200

    Fix wget error

--- setproxy.sh
+++ setproxy.sh
@@ -11,10 +11,16 @@ if [ $choice -eq 1 ] ; then
     echo
     git config --global http.proxy http://'students\'$username:$password@proxyss.wits.ac.za:80
     git config --global https.proxy http://'students\'$username:$password@proxyss.wits.ac.za:80
-    export http_proxy=\"http://'students\'$username:$password@proxyss.wits.ac.za:80\"
-    export https_proxy=\"http://'students\'$username:$password@proxyss.wits.ac.za:80\"
-    export ftp_proxy=\"http://'students\'$username:$password@proxyss.wits.ac.za:80\"
-    export all_proxy=\"http://'students\'$username:$password@proxyss.wits.ac.za:80\"
+    export http_proxy=http://'students\'$username:$password@proxyss.wits.ac.za:80
+    export no_proxy=localhost,127.0.0.1,.wits.ac.za
+    export https_proxy=$http_proxy
+    export HTTP_PROXY=$http_proxy
+    export HTTPS_PROXY=$http_proxy
+    export NO_PROXY=$no_proxy
+    export https_proxy=$http_proxy
+    export ftp_proxy=$http_proxy
+    export all_proxy=$http_proxy
+    echo "Proxy configured"
     echo
     
 else
