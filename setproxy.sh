755b3ec399c55babda86b0325c7023b3a5028ce1
Daniel Ng See Cheong <danielpnsc@gmail.com>
2017-02-16 14:26:08 +0200

    Added more proxy variables

--- setproxy.sh
+++ setproxy.sh
@@ -12,13 +12,16 @@ if [ $choice -eq 1 ] ; then
     git config --global http.proxy http://'students\'$username:$password@proxyss.wits.ac.za:80
     git config --global https.proxy http://'students\'$username:$password@proxyss.wits.ac.za:80
     export http_proxy=http://'students\'$username:$password@proxyss.wits.ac.za:80
-    export no_proxy=localhost,127.0.0.1,.wits.ac.za
-    export https_proxy=$http_proxy
     export HTTP_PROXY=$http_proxy
-    export HTTPS_PROXY=$http_proxy
+    export no_proxy=localhost,127.0.0.1,.wits.ac.za
     export NO_PROXY=$no_proxy
+    export https_proxy=$http_proxy
+    export HTTPS_PROXY=$http_proxy
     export ftp_proxy=$http_proxy
+    export FTP_PROXY=$http_proxy
     export all_proxy=$http_proxy
+    export ALL_PROXY=$http_proxy
+
     echo "Proxy configured"
     echo
     
@@ -28,10 +31,13 @@ else
 	git config --global --unset http.proxy
 	git config --global --unset https.proxy
 	unset http_proxy
+        unset HTTP_PROXY
 	unset https_proxy
+        unset HTTPS_PROXY
 	unset ftp_proxy
+	unset FTP_PROXY
 	unset all_proxy
-	unset HTTP_PROXY
-    	unset HTTPS_PROXY
+	unset ALL_PROXY
     	unset NO_PROXY
+	unset no_proxy
 fi
